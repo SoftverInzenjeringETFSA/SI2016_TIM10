e176a0e8387c8c746f91ab6b0550519829cb9357
epajic16747 <epajic2@etf.unsa.ba>
2017-05-11 16:20:25 +0200

    Update 1.2

---
+++ Projektni zadatak 5/Backend/NekretnineSI/src/main/java/com/example/NekretnineSiApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NekretnineSiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NekretnineSiApplication.class, args);
+	}
+}
