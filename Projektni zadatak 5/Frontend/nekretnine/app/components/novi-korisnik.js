229ca5564ff555a668c8ddd2dc9b743b6c3bb733
scatic1 <scatic1@etf.unsa.ba>
2017-05-21 19:44:27 +0200

    zapoceta registracija i validacija

---
+++ Projektni zadatak 5/Frontend/nekretnine/app/components/novi-korisnik.js
@@ -0,0 +1,37 @@
+import Ember from 'ember';
+import DS from 'ember-data';
+
+
+export default Ember.Component.extend({
+	errors:DS.Errors.create(),
+	actions:{
+		submit:function(){
+			if(this.validate()){
+			this.sendAction('action',this.korisnik);
+
+		}
+		}
+	},
+	
+	validate: function(){
+		this.set('errors',DS.Errors.create());
+
+		if(this.get('korisnik.ime')==='' || this.get('korisnik.ime')===undefined){
+			this.get('errors').add('ime','Unesite ime!!');
+		}
+		if(this.get('korisnik.prezime')==='' || this.get('korisnik.prezime')===undefined){
+			this.get('errors').add('prezime','Unesite prezime!!');
+		}
+		if(this.get('korisnik.username')==='' || this.get('korisnik.username')===undefined){
+			this.get('errors').add('username','Unesite username!!');
+		}
+		if(this.get('korisnik.password')==='' || this.get('korisnik.password')===undefined){
+			this.get('errors').add('password','Unesite password!!');
+		}
+		if(this.get('korisnik.email')==='' || this.get('korisnik.email')===undefined){
+			this.get('errors').add('email','Unesite email!!');
+		}
+		
+		return this.get('errors.isEmpty');
+	}
+});
