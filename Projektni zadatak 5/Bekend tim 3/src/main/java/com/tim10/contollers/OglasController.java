0e3f94759b419060a97216bb06cb21db35bdc54e
Zerina Dragnic <zdragnic@gmail.com>
2017-05-24 00:12:08 +0200

    dodana defaultna slika za oglase

--- Projektni zadatak 5/Bekend tim 3/src/main/java/com/tim10/contollers/OglasController.java
+++ Projektni zadatak 5/Bekend tim 3/src/main/java/com/tim10/contollers/OglasController.java
@@ -1,13 +1,19 @@
 package com.tim10.contollers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tim10.models.Oglas;
 import com.tim10.repositories.OglasRepozitorij;
 
+
+import java.util.List;
+
 @RestController
 @RequestMapping("/oglasi")
 public class OglasController extends BaseRestController<Oglas, OglasRepozitorij>{
 
+        
 }
--- Projektni zadatak 5/Bekend tim 3/src/main/java/com/tim10/repositories/OglasRepozitorij.java
+++ Projektni zadatak 5/Bekend tim 3/src/main/java/com/tim10/repositories/OglasRepozitorij.java
@@ -1,9 +1,13 @@
 package com.tim10.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.tim10.models.Oglas;
+import org.springframework.data.repository.query.Param;
 
-public interface OglasRepozitorij extends CrudRepository<Oglas, Long>{
+import java.util.List;
+
+public interface OglasRepozitorij extends CrudRepository<Oglas, Long> {
 
 }
